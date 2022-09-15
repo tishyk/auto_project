f9b011fe2adea0750ad2006e45616045090cd91d
Tischenko Sergiy <sytischenko@gmail.com>
2022-09-15 18:35:43 +0300

    Create common_functions.groovy

---
+++ common_functions.groovy
@@ -0,0 +1,27 @@
+import hudson.model.User
+
+HTML_REPORT_PATH = ""
+GITHUB_USERNAME = "tishyk"
+KNOWN_BRANCHES = ["main", "develop"]
+
+def show_branches(branches) {
+  for (branch in branches) {
+    pritnl("${branch}")
+    }
+}
+
+def admin_user(user){
+  return hudson.model.User.get("admin")
+}
+
+def check_branch(branch){
+  def known_branch = false
+  if (branch in KNOWN_BRANCHES) {
+    println("Branch ${branch} is in known branches list") 
+    known_branch = true
+  }
+  else {
+      println("Branch ${branch} is in known branches list")  
+  }
+  return known_branch
+}
